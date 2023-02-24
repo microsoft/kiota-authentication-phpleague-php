6f1fc034c48afd25212d02ecd8dbde9ff856f9a2
Robertbaelde <robert_baelde@hotmail.com>
2023-02-24 15:23:41 +0100

    Add docs

--- src/Cache/AccessTokenCache.php
+++ src/Cache/AccessTokenCache.php
@@ -4,9 +4,27 @@ namespace Microsoft\Kiota\Authentication\Cache;
 
 use League\OAuth2\Client\Token\AccessToken;
 
+/**
+ * Interface AccessTokenCache
+ * @package Microsoft\Kiota\Authentication
+ * @copyright 2022 Microsoft Corporation
+ * @license https://opensource.org/licenses/MIT MIT License
+ * @link https://developer.microsoft.com/graph
+ */
 interface AccessTokenCache
 {
+    /**
+     * Return cached access token if available, else return null
+     *
+     * @return AccessToken|null
+     */
     public function getAccessToken(): ?AccessToken;
 
+    /**
+     * Persist access token in cache
+     *
+     * @param AccessToken $accessToken
+     * @return void
+     */
     public function persistAccessToken(AccessToken $accessToken): void;
 }
--- src/Cache/InMemoryAccessTokenCache.php
+++ src/Cache/InMemoryAccessTokenCache.php
@@ -4,6 +4,16 @@ namespace Microsoft\Kiota\Authentication\Cache;
 
 use League\OAuth2\Client\Token\AccessToken;
 
+/**
+ * Class InMemoryAccessTokenCache
+ *
+ * In memory cache for access token
+ *
+ * @package Microsoft\Kiota\Authentication
+ * @copyright 2022 Microsoft Corporation
+ * @license https://opensource.org/licenses/MIT MIT License
+ * @link https://developer.microsoft.com/graph
+ */
 class InMemoryAccessTokenCache implements AccessTokenCache
 {
     private ?AccessToken $accessToken = null;
