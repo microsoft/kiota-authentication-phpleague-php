3cc2801a1f59721f2ac1c558ea7dcaba5882e6db
Philip Gichuhi <pgichuhi@microsoft.com>
2023-05-29 13:07:29 +0300

    Allow changing default token service host and user info host

--- tests/Oauth/ProviderFactoryTest.php
+++ tests/Oauth/ProviderFactoryTest.php
@@ -3,13 +3,16 @@
 namespace Microsoft\Kiota\Authentication\Test\Oauth;
 
 use GuzzleHttp\Client;
+use InvalidArgumentException;
+use League\OAuth2\Client\Provider\GenericProvider;
+use League\OAuth2\Client\Token\AccessToken;
 use Microsoft\Kiota\Authentication\Oauth\ClientCredentialContext;
 use Microsoft\Kiota\Authentication\Oauth\ProviderFactory;
 use PHPUnit\Framework\TestCase;
 
 class ProviderFactoryTest extends TestCase
 {
-    public function testCustomHttpClient()
+    public function testCustomHttpClient(): void
     {
         $httpClient = new Client();
 
@@ -21,5 +24,33 @@ class ProviderFactoryTest extends TestCase
         );
 
         self::assertSame($httpClient, $provider->getHttpClient());
+    }
+
+    public function testDefaultConfiguration(): void
+    {
+        $oauthProvider = ProviderFactory::create(new ClientCredentialContext(
+            '1', 'client', 'secret'
+        ));
+        $this->assertInstanceOf(GenericProvider::class, $oauthProvider);
+        $this->assertEquals("https://graph.microsoft.com/oidc/userinfo", $oauthProvider->getResourceOwnerDetailsUrl(
+            $this->createMock(AccessToken::class)
+        ));
+        $this->assertEquals("https://login.microsoftonline.com/1/oauth2/v2.0/token", $oauthProvider->getBaseAccessTokenUrl([]));
+        $this->assertEquals("https://login.microsoftonline.com/1/oauth2/v2.0/authorize", $oauthProvider->getBaseAuthorizationUrl());
+    }
+
+    public function testUpdatingBaseURLs(): void
+    {
+        $chinaCloudUserInfo = 'https://microsoftgraph.chinacloudapi.cn';
+        $chinaCloudTokenService = 'https://login.chinacloudapi.cn';
+        $oauthProvider = ProviderFactory::create(new ClientCredentialContext(
+            '1', 'client', 'secret'
+        ), [], $chinaCloudTokenService, $chinaCloudUserInfo);
+        $this->assertInstanceOf(GenericProvider::class, $oauthProvider);
+        $this->assertEquals("$chinaCloudUserInfo/oidc/userinfo",$oauthProvider->getResourceOwnerDetailsUrl(
+            $this->createMock(AccessToken::class)
+        ));
+        $this->assertEquals("$chinaCloudTokenService/1/oauth2/v2.0/token", $oauthProvider->getBaseAccessTokenUrl([]));
+        $this->assertEquals("$chinaCloudTokenService/1/oauth2/v2.0/authorize", $oauthProvider->getBaseAuthorizationUrl());
     }
 }
